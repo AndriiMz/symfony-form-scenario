02dfd33bdd9261e5e966cd685bc8217bfa61db7f
andrii <moroz97andre@mail.ua>
2020-07-25 22:39:19 +0200

    Initial

--- src/DependencyInjection/ScenarioMutationCompilerPass.php
+++ src/DependencyInjection/ScenarioMutationCompilerPass.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-class ChangeDispatcherCompilerPass implements CompilerPassInterface
+class ScenarioMutationCompilerPass implements CompilerPassInterface
 {
     /**
      * @param ContainerBuilder $container
